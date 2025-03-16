739a4ace663b6e002746bfa2e0ab0ea7958356fd
mc-solo <revatoel@gmail.com>
2025-03-16 22:17:12 +0300

    update: auth service now uses token based approach

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,20 +5,52 @@ interface LoginCredentials {
     password: string;
 }
 
-// logs the user in using the provided credentials
+interface RegisterData {
+    first_name: string;
+    last_name: string;
+    phone_number: string;
+    email: string;
+    password: string;
+}
+
+/**
+ * logs the user in
+ * @param credentials obj with email and password
+ * @returns a promise that resolves to the login res data
+ */
 export const login = async (credentials: LoginCredentials) => {
-    api.get('/sanctum/csrf-cookie');
-    const response = await api.post('/login', 'credentials');
+    const response = await api.post('/login', credentials);
+    return response.data;   //expected: {message, user, token}
+}
+
+/**
+ * registers a new user
+ * @param data obj with register data
+ * @returns a promise that resolves to the register res data
+ */
+export const register = async(data: RegisterData) => {
+    const response = await api.post('/register', 'data');
+    return response.data;   //expected: {message, user, token}
+}
+
+/**
+ * logs the user out
+ * @param token the user's auth token
+ * @returns a promise that resolves to the logout res data
+ */
+export const logout = async (token: string) => {
+    const response = await api.post('/logout', null, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
     return response.data;
 }
 
-// logs the user out
-export const logout = async () => {
-    const response = await api.post('/logout');
-    return response.data;
-}
-
-// fetches the currently authenticated user
+/**
+ * fetches the user data
+ * @returns a promise that resolves to the user data
+ */
 export const fetchUser = async () => {
     const  response  = await api.get('/user');
     return response.data;
