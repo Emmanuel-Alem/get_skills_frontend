23849ff22e7b043cbde7b6c1f513dbf7b5115858
mc-solo <revatoel@gmail.com>
2025-03-16 22:07:09 +0300

    fix: removes withCredentials option since we gonna use token based auth

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 /**
- * Create an axios instance
+ * axios instance configured for my laravel backend
  * @param baseURL - the base URL for the backend API
  * @param withCredentials - ensures my cookies are sent with the request [incliding csrf token]
  **/
 
 const api = axios.create({
   baseURL: process.env.Next_PUBLIC_API_URL,
-  withCredentials: true,
 });
 
 
